fbcbb4ecd7e077ebe0921dd504ecc7a38f795d9b
diametric <diametric@zerotech.net>
2013-06-12 12:43:06 -0400

    Updated the README with some more info, finished implementing basic user preferences and fold all functionality.  Mostly working onload event.

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-Sublime Text Custom Folding Plugin
+# Sublime Text Custom Folding Plugin
+
+This plugin allows you to define arbitrary folding start and end tags.
 
 A simple plugin for defining artbitrary tags for folding.
 
+#### Configuration
+
+To define the tags to fold on.
+
+`"customfolding_tags": [ 
+   ["tag1open", "tag1close"],
+   ["tag2open", "tag2close"],
+   ...
+
+]`
+
+To define the tags to fold on file open.  These can be a subset, or independent
+of the defined defined customfolding_tags setting.  If these are independent 
+they will only be folded on file open, you will be unable to fold them later.
+
+`"customfolding_onload_tags": [
+   ["tag1open", "tag1close"],
+   ["tag2open", "tag2close"],
+   ...
+
+]
--- sublime_customfolding/sublime_customfolding.py
+++ sublime_customfolding/sublime_customfolding.py
@@ -7,7 +7,93 @@
 #
 #
 
-## Here's some fun testing area.
+import sublime, sublime_plugin
+
+# Assuming pos begins after an open tag, find the proper closing tag.
+def find_matching_fold(view, tagopen, tagclose, pos, nest = 0):
+	# Look for the next open and close tags that aren't in a string. 
+	opos, cpos = pos, pos
+	while True:
+		nextopen = view.find(tagopen, opos, sublime.LITERAL)
+		if view.score_selector(nextopen.a, "string") == 0:
+			break
+		else:
+			opos = nextopen.b
+
+	while True:
+		nextclose = view.find(tagclose, cpos, sublime.LITERAL)
+		if view.score_selector(nextclose.a, "string") == 0:
+			break
+		else:
+			cpos = nextclose.b
+
+	# No more matching close tags, just fold it all up.
+	if nextclose.empty():
+		return sublime.Region(view.size(), view.size())
+
+	# Found another open tag before we found the matching close tag
+	if not nextopen.empty() and nextopen.b < nextclose.b:
+		nest = nest + 1
+		return find_matching_fold(view, tagopen, tagclose, nextopen.b, nest)
+	# Found a close tag
+	else:
+		# Unroll it
+		if nest > 0:
+			nest = nest - 1
+			return find_matching_fold(view, tagopen, tagclose, nextclose.b, nest)
+		# Match
+		else:
+			return nextclose
+
+# Finds the first open tag starting at the beginning of the view, and then
+# recursively closes every custom fold region.
+
+def fold_tag_all(view, opentag, closetag):
+	p = 0
+	while True:
+		firstopen = view.find(opentag, p, sublime.LITERAL)
+
+		# No folding, just return.
+		if firstopen.empty():
+			return
+
+		p = firstopen.b
+
+		# Ignore fold markers inside of strings. 
+		if view.score_selector(firstopen.a, "string") > 0:
+			print("Skipping over string fold")
+			continue
+
+		firstclose = find_matching_fold(view, opentag, closetag, firstopen.b)
+		foldregion = sublime.Region(firstopen.b, firstclose.a)
+
+		print("Folding region %s" % foldregion)
+
+		view.fold(foldregion)
+
+
+def fold_ALL_the_things(view):
+	for tags in view.settings().get("customfolding_onload_tags", []):
+		fold_tag_all(view, tags[0], tags[1])
+
+# Handles folding tags on file load
+class CustomfoldingEventListener(sublime_plugin.EventListener):
+	def on_load(self, view):
+		if view.settings().get("customfolding_onload_all", False):
+			fold_ALL_the_things(view)
+
+		defaultFolds = view.settings().get("customfolding_onload_tags", [])
+
+
+		if len(view.settings().get("customfolding_onload_tags", [])) > 0:
+			fold_ALL_the_things(view)
+
+
+### Test Area
+
+### VERY_LONG_AND_ANNOYING_FOLD_TAG_START
+
+## Some unrelated content.
 
 # {{{ fold 1
 
@@ -25,60 +111,6 @@
 
 # }}} endfold 2
 
-import sublime, sublime_plugin
+# }}}
 
-def find_matching_fold(view, tagopen, tagclose, pos, nest = 0):
-	while True:
-		nextopen = view.find(tagopen, pos, sublime.LITERAL)
-		if view.score_selector(nextopen.a, "string") == 0:
-			break
-		else:
-			pos = nextopen.b
-
-	while True:
-		nextclose = view.find(tagclose, pos, sublime.LITERAL)
-		if view.score_selector(nextclose.a, "string") == 0:
-			break
-		else:
-			pos = nextclose.b
-
-	# No more matching close tags, just fold it all up.
-	if nextclose.empty():
-		return sublime.Region(view.size(), view.size())
-
-	if not nextopen.empty() and nextopen.b < nextclose.b:
-		nest = nest + 1
-		return find_matching_fold(view, tagopen, tagclose, nextopen.b, nest)
-	else:
-		if nest > 0:
-			nest = nest - 1
-			return find_matching_fold(view, tagopen, tagclose, nextclose.b, nest)
-		else:
-			return nextclose
-
-def fold_ALL_the_things(view):
-	p = 0
-	while True:
-		firstopen = view.find("{{{", p, sublime.LITERAL)
-
-		# No folding, just return.
-		if firstopen.empty():
-			return
-
-		p = firstopen.b
-
-		# Ignore fold markers inside of strings. 
-		if view.score_selector(firstopen.a, "string") > 0:
-			print("Skipping over string fold")
-			continue
-
-		firstclose = find_matching_fold(view, "{{{", "}}}", firstopen.b)
-		foldregion = sublime.Region(firstopen.b, firstclose.a)
-
-		print("Folding region %s" % foldregion)
-
-		view.fold(foldregion)
-
-
-
-
+### VERY_LONG_AND_ANNOYING_FOLD_TAG_END
